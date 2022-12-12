6ffe21fd90e94d5587b5e1d66894e1a786913016
btierny <bertille.tierny@ensae.fr>
2022-12-12 16:53:52 +0000

    changements du jour

--- functions_for_modelling.py
+++ functions_for_modelling.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Created By  : name_of_the_creator   
+# Created Date: date/month/time ..etc
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Details about the module and for what purpose it was built for"""  #Line 4
+# ---------------------------------------------------------------------------
+# Imports Line 5
+# ---------------------------------------------------------------------------
+#from ... import ...  #Line 6
 """
 function for data cleaning 
 """
